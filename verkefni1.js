d8ecfcf4e3f98233f46fe7274b62d09248d7faae
SindriSSH <sindrihj305@nemi.tskoli.is>
2017-01-13 12:39:38 +0000

    Add files via upload

---
+++ verkefni1.js
@@ -0,0 +1,12 @@
+
+document.write("<h1>Hello World!</h1>");
+
+var budarlisti = new Array();
+budarlisti[0] = 'egg';
+budarlisti[1] = 'mjolk';
+budarlisti[2] = 'hamborgari';
+budarlisti[3] = 'epli';
+budarlisti[4] = 'svali';
+for (i=0;i<budarlisti.length;i++) {
+	 document.write(budarlisti[i] + "<br >"); 
+}
